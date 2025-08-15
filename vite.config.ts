d0ff5a0367c97d43335701c7706de4fdc146afcf
Oladipupo Olubunmi <74682430+Bunmi2020@users.noreply.github.com>
2025-08-15 03:39:11 +0100

    chnage base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // <-- This is the important change
   server: {
     host: "::",
     port: 8080,
@@ -14,8 +14,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
